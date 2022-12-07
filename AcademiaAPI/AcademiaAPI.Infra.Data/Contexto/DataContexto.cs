8a78fb989535ae94cf09f739d668cdaff4b4a048
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-12-07 06:03:34 -0300

    Criação do projeto junto com toda estrutura de pacote e criação do contexto

---
+++ AcademiaAPI/AcademiaAPI.Infra.Data/Contexto/DataContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaAPI.Infra.Data.Contexto
+{
+    public class DataContexto : DbContext
+    {
+        public DataContexto(DbContextOptions<DataContexto> options) : base(options){}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ThiagoApis.NetCore
 Repositório que vai conter todas as APIS que vou está criando como treinamento.
+
+
+Academia
+    youtube.com/watch?v=cH4JWX8j5jI&list=PLdpC-YBhqMDz8tvWcoJJUyv6QhnjzxDIe
